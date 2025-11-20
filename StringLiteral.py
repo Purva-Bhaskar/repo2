d6729c7535ed3e2dba9a7276523b9bce0a8b3ad7
Purva-Bhaskar <bhaskarpurva2415@gmail.com>
2025-11-20 10:36:49 +0530

    Add files via upload

---
+++ StringLiteral.py
@@ -0,0 +1,16 @@
+str1="Hello!"
+str2='Welcome to FCT'
+str3="""Python Session
+this is day 2 in python learing """
+
+print(str1)
+print(str2)
+print(str3)
+
+concatenation=str1+str2
+repeated= str1*3
+print(concatenation)
+print(repeated)
+
+str4="This is example of \'Escape Character\'\nThis is new line"
+print(str4)
